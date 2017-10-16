a10f68ff070a9e5d1d24aac00a4692810f62633d
naderae <naderabouezze93@gmail.com>
2017-10-16 13:48:03 -0400

    added login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
 
   def new
-    
+
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/bikes'
+    else
+      redirect_to '/'
+    end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   root 'users#new'
 
+  resource :users, :only => [:new, :create]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :bikes, only: [:index]
 
-  resource :users, :only => [:new, :create]
+
 end
